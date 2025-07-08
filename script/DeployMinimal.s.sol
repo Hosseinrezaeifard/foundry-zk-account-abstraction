dd7e1972cc7ae779890e0ed5c4817fdb0bc0fe2e
HosseinRezaeiFard <huseinrezaeif99@gmail.com>
2025-07-08 14:13:13 -0700

    Implement deploy script

---
+++ script/DeployMinimal.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {MinimalAccount} from "../src/eth/MinimalAccount.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+
+contract DeployMinimal is Script {
+    address public entryPoint;
+
+    function run() public {
+        deployMinimalAccount();
+    }
+
+    function deployMinimalAccount()
+        public
+        returns (HelperConfig, MinimalAccount)
+    {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+
+        vm.startBroadcast(config.account);
+        MinimalAccount minimalAccount = new MinimalAccount(config.entryPoint);
+        minimalAccount.transferOwnership(msg.sender);
+        vm.stopBroadcast();
+        return (helperConfig, minimalAccount);
+    }
+}
