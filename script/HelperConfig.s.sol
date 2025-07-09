c4d1b403597c1ba0609c2ed490f0933ebecc7682
HosseinRezaeiFard <huseinrezaeif99@gmail.com>
2025-07-09 12:41:19 -0700

    Implement testRecoverSignedOp

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,7 +2,8 @@
 
 pragma solidity ^0.8.20;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console2} from "forge-std/Script.sol";
+import {EntryPoint} from "lib/account-abstraction/contracts/core/EntryPoint.sol";
 
 contract HelperConfig is Script {
     error HelpConfig__InvalidChainId();
@@ -16,8 +17,10 @@ contract HelperConfig is Script {
     uint256 constant ZKSYNC_SEPLOIA_CHAIN_ID = 300;
     uint256 constant LOCAL_CHAIN_ID = 31337;
     address constant BURNER_WALLET = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-    address constant FOUNDRY_DEFAULT_WALLET =
-        0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
+    // address constant FOUNDRY_DEFAULT_WALLET =
+    //     0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
+    address constant ANVIL_DEFAULT_ACCOUNT =
+        0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
 
     NetworkConfig public localNetworkConfig;
     mapping(uint256 chainId => NetworkConfig) public networkConfigs;
@@ -63,10 +66,16 @@ contract HelperConfig is Script {
         if (localNetworkConfig.account != address(0)) {
             return localNetworkConfig;
         }
-        return
-            NetworkConfig({
-                entryPoint: address(0),
-                account: FOUNDRY_DEFAULT_WALLET
-            });
+
+        console2.log("Deploying mocks...");
+        vm.startBroadcast(ANVIL_DEFAULT_ACCOUNT);
+        EntryPoint entryPoint = new EntryPoint();
+        vm.stopBroadcast();
+
+        localNetworkConfig = NetworkConfig({
+            entryPoint: address(entryPoint),
+            account: ANVIL_DEFAULT_ACCOUNT
+        });
+        return localNetworkConfig;
     }
 }
--- test/eth/MinimalAccount.t.sol
+++ test/eth/MinimalAccount.t.sol
@@ -7,18 +7,25 @@ import {MinimalAccount} from "src/eth/MinimalAccount.sol";
 import {DeployMinimal} from "script/DeployMinimal.s.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {SendPackedUserOp, PackedUserOperation} from "script/SendPackedUserOp.s.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 contract MinimalAccountTest is Test {
+    using MessageHashUtils for bytes32;
     HelperConfig public helperConfig;
     MinimalAccount public minimalAccount;
     ERC20Mock usdc;
     uint256 constant AMOUNT = 1e18;
     address public randomUser = makeAddr("randomUser");
+    SendPackedUserOp public sendPackedUserOp;
 
     function setUp() public {
         DeployMinimal deployMinimal = new DeployMinimal();
         (helperConfig, minimalAccount) = deployMinimal.deployMinimalAccount();
         usdc = new ERC20Mock();
+        sendPackedUserOp = new SendPackedUserOp();
     }
 
     function testOwnerCanExecute() public {
@@ -56,5 +63,35 @@ contract MinimalAccountTest is Test {
         minimalAccount.execute(dest, value, funcData);
         // Assert
         assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+    }
+
+    function testRecoverSignedOp() public {
+        // Arrange
+        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory funcData = abi.encodeWithSelector(
+            ERC20Mock.mint.selector,
+            address(minimalAccount),
+            AMOUNT
+        );
+        // this variable is saying hey entrypoint contract, call our contract, then our contract call usdc
+        bytes memory executeCallData = abi.encodeWithSelector(
+            MinimalAccount.execute.selector,
+            dest,
+            value,
+            funcData
+        );
+        PackedUserOperation memory userOp = sendPackedUserOp
+            .generateSignedUserOp(executeCallData, helperConfig.getConfig());
+        bytes32 userOpHash = IEntryPoint(helperConfig.getConfig().entryPoint)
+            .getUserOpHash(userOp);
+        // Act
+        address actualSigner = ECDSA.recover(
+            userOpHash.toEthSignedMessageHash(),
+            userOp.signature
+        );
+        // Assert
+        assertEq(actualSigner, minimalAccount.owner());
     }
 }
